4d1734dbe0f10a15878b7f497458d2557a2106dc
evasalgado <imviarsha@gmail.com>
2024-03-01 14:31:46 -0600

    Cpp agregado solo uno

--- CuentaAhorro.cpp
+++ CuentaAhorro.cpp
@@ -1,1 +1,5 @@
 #include "CuentaAhorro.h"
+CuentaAhorro::CuentaAhorro(double balance, int NumCuenta, string Nombre, double TasaInteres):
+CuentaBancaria(balance, NumCuenta, TasaInteres){
+
+}
--- CuentaBancaria.cpp
+++ CuentaBancaria.cpp
@@ -1,1 +1,35 @@
 #include "CuentaBancaria.h"
+CuentaBancaria::CuentaBancaria(double balance, int NumCuenta, string Nombre) {
+	this->balance = balance;
+	this->NumCuenta = NumCuenta;
+	this->Nombre = Nombre;
+}
+double CuentaBancaria::getBalance() {
+	return balance;
+}
+void CuentaBancaria::setBalance(double &balance) {
+	this->balance = balance;
+}
+int CuentaBancaria::getNumCuenta() {
+	return NumCuenta;
+}
+void CuentaBancaria::setNumCuenta(int &NumCuenta) {
+	this->NumCuenta = NumCuenta;
+}
+string CuentaBancaria::getNombre() {
+	return Nombre;
+}
+void CuentaBancaria::setNombre(string& Nombre) {
+	this->Nombre = Nombre;
+}
+CuentaBancaria::~CuentaBancaria() {
+	balance = 0;
+	NumCuenta = 0;
+	Nombre = "";
+}
+void CuentaBancaria::depositar(double monto) const {
+	monto = 0.0;
+}
+void CuentaBancaria::retirar(double monto) const {
+	monto = 0.0;
+}
